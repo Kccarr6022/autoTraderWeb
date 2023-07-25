efffb8b5ab46ddeb7e9254f7c22636d47faf8f6a
Kaden <kccarr6022@eagle.fgcu.edu>
2023-07-24 23:07:15 -0400

    Setup of react app and blank python api

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <section>
+      <header className="bg-sky-400 py-4">
+        <h1 className="text-3xl font-bold underline text-center">
+          Auto Trader
+        </h1>
+      </header>
+      <main></main>
+    </section>
+  );
+}
+
+export default App;
